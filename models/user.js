b441bfacea4a1ca3c30bebd7a2ca488d7e855160
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-08-02 16:45:31 +0100

    ex

--- models/user.js
+++ models/user.js
@@ -5,7 +5,12 @@ const userSchema = mongoose.Schema({
   createdAt: { type: Date, required: true },
   userFirstName: { type: String, required: true },
   userLastName: { type: String, required: true },
-  userEmail: { type: String, required: true, unique: true },
+  userEmail: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+  },
   userPassword: { type: String, required: true },
 });
 
