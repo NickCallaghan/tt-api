f2aaf9b73ddc230a1643dbe7f62c3ece8f5cccf2
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-08-02 16:37:39 +0100

    Add create user route

--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   createdAt: { type: Date, required: true },
-  userName: { type: String, required: true },
+  userFirstName: { type: String, required: true },
+  userLastName: { type: String, required: true },
   userEmail: { type: String, required: true },
   userPassword: { type: String, required: true },
 });
--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,51 @@
 require("dotenv");
 
-const APP_PORT = process.env.APP_PORT;
-const APP_BASEURL = process.env.APP_BASEURL;
-
-const url = `${APP_BASEURL}:${APP_PORT}`;
-const route = "user";
-
+// Libraries
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+// Mongoose Models
 const User = require("../models/user");
 
-router.post("/signup", (req, res, next) => {
-  res.status(201).json({ message: "New User Created" });
+// POST - Create User / Sign Up Route
+router.post("/signup", async (req, res, next) => {
+  // Check if user email already exists
+  const matchedUsers = await User.find({
+    userEmail: req.body.userEmail,
+  }).exec();
+
+  if ((await matchedUsers.length) >= 1) {
+    return res.status(422).json({ message: "User email already exists" });
+  } else {
+    // Create new user object
+    const { userFirstName, userLastName, userEmail, userPassword } = req.body;
+
+    // Create password hash
+    await bcrypt.hash(userPassword, 10, async (err, hash) => {
+      try {
+        const user = new User({
+          _id: new mongoose.Types.ObjectId(),
+          createdAt: Date.now(),
+          userFirstName,
+          userLastName,
+          userEmail,
+          userPassword: hash,
+        });
+
+        // Save new user to db
+        const result = await user.save();
+        res.status(201).json({
+          message: "user Created",
+          user: result,
+        });
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err });
+      }
+    });
+  }
 });
 
 router.post("/signup", (req, res, next) => {
